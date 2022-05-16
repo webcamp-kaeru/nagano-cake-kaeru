d81873731cb7418993206be0f0edaecdc7f5c75d
EC2 Default User <ec2-user@ip-172-31-6-225.ec2.internal>
2022-05-16 09:25:55 +0000

    会員の新規登録、ログインページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+
+  root to: "public/homes#top"
+  get "about" => 'public/homes#about'
   
+
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
