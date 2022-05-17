71030485c5e8faa4b0c47748084360990fe5f768
EC2 Default User <ec2-user@ip-172-31-45-54.ec2.internal>
2022-05-17 05:39:23 +0000

    追記

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,9 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "public/homes#top"
+  get "about" => 'public/homes#about'
+
+
+  devise_for :admins
 end
