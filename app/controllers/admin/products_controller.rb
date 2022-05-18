12534c5e918e6b4c7de61294bf39bce6b6983bb0
Emiri Maegawa <101238309+mgwem@users.noreply.github.com>
2022-05-18 06:03:51 +0000

    Add newページレイアウト,コントローラ

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -8,15 +8,29 @@ class Admin::ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+    # @genres = Genres.all
   end
 
   def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to admin_products_path
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to edit_admin_product_path(@product)
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :text, :genre_id, :price, :sales_status, :product_image)
   end
 
 end
