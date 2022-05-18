9a479b3a0772406825efa958fe9f970a991efad7
EC2 Default User <ec2-user@ip-172-31-45-54.ec2.internal>
2022-05-18 10:25:33 +0000

    会員情報

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
 
   belongs_to :genre
-  has_many :order_products, dependent: :des
-  
+  has_many :order_products, dependent: :destroy
+
   has_one_attached :product_image
 
   def sum_of_order_price
