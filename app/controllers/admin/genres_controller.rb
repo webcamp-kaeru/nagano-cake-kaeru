8e181af284ea6ab9ef41504fd3b91e592f105242
EC2 Default User <ec2-user@ip-172-31-45-54.ec2.internal>
2022-05-18 05:44:06 +0000

    修正

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -23,7 +23,7 @@ class Admin::GenresController < ApplicationController
     @genre = Genre.find(params[:id])
     if @genre.update(genre_params)
       flash[:notice] = "ジャンル名を保存しました"
-      redirect_to admin_genre_path
+      redirect_to admin_genres_path
     else
       render :edit
     end
