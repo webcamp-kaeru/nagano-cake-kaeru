2309034f902066cb29da48de10dc6a1ad16a8d8d
EC2 Default User <ec2-user@ip-172-31-6-225.ec2.internal>
2022-05-19 18:19:13 +0000

    配送先のページの追加

--- app/controllers/public/delivery_addresses_controller.rb
+++ app/controllers/public/delivery_addresses_controller.rb
@@ -1,9 +1,20 @@
 class Public::DeliveryAddressesController < ApplicationController
   layout 'public/application'
   def index
+    @delivery_address = DeliveryAddress.new
+    @delivery_addresses = current_member.delivery_addresses
   end
 
   def create
+    @delivery_address = DeliveryAddress.new(delivery_address_params)
+    @delivery_address.member_id = current_member.id
+    if @delivery_address.save
+      flash[:notice] = "配送先を登録しました"
+      redirect_back fallback_location: root_path
+    else
+      flash.now[:alert] = '配送先の登録に失敗しました'
+      render :index
+    end
   end
 
   def edit
@@ -14,4 +25,11 @@ class Public::DeliveryAddressesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def delivery_address_params
+    params.require(:delivery_address).permit(:postal_code, :address, :shipping_name)
+  end
+
 end
