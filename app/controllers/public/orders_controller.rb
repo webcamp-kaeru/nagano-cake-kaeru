946ad012f5cf6c96d05ae39e29fe57dd520ed752
Emiri Maegawa <101238309+mgwem@users.noreply.github.com>
2022-05-20 10:48:56 +0000

    Add 注文機能confirmページ

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -13,12 +13,10 @@ class Public::OrdersController < ApplicationController
   def confirm
     @order = Order.new(order_params)
     if params[:order][:select_address] == "1"
-      # ご自身の住所
       @order.shipping_postal_code = current_member.postal_code
       @order.shipping_address = current_member.address
       @order.shipping_name = current_member.last_name + current_member.first_name
     elsif params[:order][:select_address] == "2"
-      # 登録済住所
       @address = DeliveryAddress.find(params[:order][:address_id])
       @order.shipping_postal_code = @address.postal_code
       @order.shipping_address = @address.address
