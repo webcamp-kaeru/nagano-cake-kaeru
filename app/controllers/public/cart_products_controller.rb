59047e386e962a23a3d7a715a95caf64fc38bc5e
EC2 Default User <ec2-user@ip-172-31-6-225.ec2.internal>
2022-05-20 10:45:35 +0000

    ほぼ完成

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -2,16 +2,14 @@ class Public::CartProductsController < ApplicationController
   layout 'public/application'
 
   def index
-    @cart_products = CartProduct.all
+    @cart_products = current_member.cart_products
     @total_price = 0
   end
 
   def create
-    @cart_product = CartProduct.new
-    @product = Product.find(cart_product_params[:product_id])
+    @cart_product = CartProduct.new(cart_product_params)
     @cart_product.member_id = current_member.id
-    @cart_product.product_id = @product.id
-    if @product.save
+    if @cart_product.save
       redirect_to products_path
     else
       render :"products/show"
