9acaa3e54300762a9e71802d8d0e1b917a60d7b1
EC2 Default User <ec2-user@ip-172-31-6-225.ec2.internal>
2022-05-20 09:28:04 +0000

    カート機能途中

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,10 +1,21 @@
 class Public::CartProductsController < ApplicationController
   layout 'public/application'
+
   def index
-    @cart_product = CartProduct.new
+    @cart_products = CartProduct.all
+    @total_price = 0
   end
 
   def create
+    @cart_product = CartProduct.new
+    @product = Product.find(cart_product_params[:product_id])
+    @cart_product.member_id = current_member.id
+    @cart_product.product_id = @product.id
+    if @product.save
+      redirect_to products_path
+    else
+      render :"products/show"
+    end
   end
 
   def update
@@ -14,5 +25,10 @@ class Public::CartProductsController < ApplicationController
   end
 
   def destroy_all
+  end
+
+  private
+  def cart_product_params
+    params.require(:cart_product).permit(:product_id, :quantity)
   end
 end
