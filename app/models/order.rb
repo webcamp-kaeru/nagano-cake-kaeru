e6b044b26143b3188aedb247835bc55aa9508680
EC2 Default User <katsura2106@outlook.jp>
2022-05-20 06:43:27 +0000

    注文履歴一覧・詳細、ありがとうページ追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,12 @@
 class Order < ApplicationRecord
-  
+
   belongs_to :member
   has_many :order_products, dependent: :destroy
-  
+
+  def sum_of_order_price
+    (billing_amount - 800).round
+  end
+
+
+
 end
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -3,4 +3,8 @@ class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
+    def quantity_price
+      (price * quantity).round
+    end
+
 end
