80698a549798962bea22e98673788bda95322673
EC2 Default User <ec2-user@ip-172-31-6-225.ec2.internal>
2022-05-21 05:07:18 +0000

    カート機能作成

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -17,16 +17,41 @@ class Public::CartProductsController < ApplicationController
   end
 
   def update
+    @cart_product = CartProduct.find(params[:id])
+      if @cart_product.update(cart_product_params)
+        flash[:notice] = "{@cart_product.name}の数量を変更しました"
+        redirect_back fallback_location: root_path
+      else
+        flash.now[:alert] = '{@cart_product.name}の数量を変更できませんでした'
+        redirect_back fallback_location: root_path
+      end
   end
 
   def destroy
+    @cart_product = CartProduct.find(params[:id])
+      if @cart_product.destroy
+        flash[:notice] = "カートから{@cart_product.name}を削除しました"
+        redirect_back fallback_location: root_path
+      else
+        flash.now[:alert] = 'カートから{@cart_product.name}を削除できませんでした'
+        redirect_back fallback_location: root_path
+      end
   end
 
   def destroy_all
+    destroy_all = current_member.cart_products
+    if destroy_all.destroy_all
+      flash[:notice] = "カートを空にしました"
+      redirect_back fallback_location: root_path
+    else
+      flash.now[:alert] = 'カートを空にできませんでした'
+      redirect_back fallback_location: root_path
+    end
   end
 
   private
-  def cart_product_params
-    params.require(:cart_product).permit(:product_id, :quantity)
-  end
+
+    def cart_product_params
+      params.require(:cart_product).permit(:product_id, :quantity)
+    end
 end
