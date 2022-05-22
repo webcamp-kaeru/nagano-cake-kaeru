be0eec3338c3d0a93da27ae6b9ae00eb18f89fe6
Emiri Maegawa <101238309+mgwem@users.noreply.github.com>
2022-05-22 11:05:17 +0000

    注文・ジャンルのバリデーション

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,15 @@ class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum order_status: { wait_payment: 0, confirm_payment: 1, making: 2, preparing_ship: 3, sent: 4 }
 
+  validates :member_id, presence: true
+  validates :shipping, presence: true
+  validates :shipping_postal_code, presence: true
+  validates :shipping_address, presence: true
+  validates :shipping_name, presence: true
+  validates :payment_method, presence: true
+  validates :order_status, presence: true
+  validates :billing_amount, presence: true
+  
   def sum_of_order_price
     (billing_amount - shipping).round
   end
