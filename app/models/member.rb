117a9528742a93f8d7c8c984f2d51f745103d567
EC2 Default User <ec2-user@ip-172-31-6-225.ec2.internal>
2022-05-22 10:27:55 +0000

    admin,publicモデルのバリデーション追加

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,4 +8,12 @@ class Member < ApplicationRecord
   has_many :delivery_addresses, dependent: :destroy
   has_many :cart_products, dependent: :destroy
 
+ validates :last_name, presence: true
+ validates :first_name, presence: true
+ validates :last_name_kana, presence: true
+ validates :first_name_kana, presence: true
+ validates :postal_code, presence: true
+ validates :address, presence: true
+ validates :phone_number, presence: true
+
 end
