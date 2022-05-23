f8bd340875b8da675af1ba17c3c05eee5c6a801b
EC2 Default User <ec2-user@ip-172-31-45-54.ec2.internal>
2022-05-23 05:47:07 +0000

    製作ステータス

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -1,8 +1,16 @@
 class Admin::OrderProductsController < ApplicationController
   def update
+    @order = Order.find(params[:order_id])
     @order_product = OrderProduct.find(params[:id])
-      if @order_product.update(order_product_params)
-        flash[:notice] = "製作ステータスを更新しました"
+    @order_products = @order.order_products
+    @order_product.update(order_product_params)
+    flash[:notice] = "製作ステータスを更新しました"
+      if @order_product.product_status == "manufacturing"
+        @order.update(order_status: "making")
+        redirect_back fallback_location: root_path
+      elsif
+        @order_products.count == @order_products.where(product_status: "finish").count
+        @order.update(order_status: "preparing_ship")
         redirect_back fallback_location: root_path
       else
         redirect_back fallback_location: root_path
