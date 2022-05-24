0d956e1b0d735b7c5d017158d705b0f5d31bb728
EC2 Default User <ec2-user@ip-172-31-6-225.ec2.internal>
2022-05-24 03:58:25 +0000

    スライドショー直った

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).on('turbolinks:load',function () {
   $("#theTarget").skippr({
     // スライドショーの変化 ("fade" or "slide")
     transition : 'slide',
@@ -7,7 +7,7 @@ $(document).ready(function () {
     // easingの種類
     easing : 'easeOutQuart',
     // ナビゲーションの形("block" or "bubble")
-    navType : 'bubble',
+    navType : 'block',
     // 子要素の種類('div' or 'img')
     childrenElementType : 'div',
     // ナビゲーション矢印の表示(trueで表示)
